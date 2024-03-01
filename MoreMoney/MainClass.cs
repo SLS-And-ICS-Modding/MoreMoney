dcf5e4ccf6ab4b371fba0ef60c4a757b1b97cda4
Zordon <65111609+Zordon1337@users.noreply.github.com>
2024-03-01 20:41:21 +0100

    proper implementation

--- MoreMoney/MainClass.cs
+++ MoreMoney/MainClass.cs
@@ -10,24 +10,40 @@ namespace MoreMoney
 {
     public class MainClass:MelonMod
     {
-        
+        float oldmoney = 0;
+        string texttoshow = "";
+        int maxframes = 3000;
+        int renderedframes = 0;
         public override void OnApplicationStart()
         {
-            
+            oldmoney = PlayerPrefs.GetFloat("money");
         }
         public override void OnUpdate()
         {
-            foreach(var civil in GameObject.FindObjectsOfType<Civil>())
+            if(PlayerPrefs.GetFloat("money") != oldmoney)
             {
-                if(civil.payedPrice > 0) 
-                {
-                    civil.payedPrice = civil.payedPrice * 2;
-                }
+                float orginalval = PlayerPrefs.GetFloat("money") - oldmoney;
+                float multipledvalue = orginalval * 2;
+                texttoshow = ($"you've earned {multipledvalue} after MoreMoney recalc");
+                renderedframes = 0;
+                float newvalue = oldmoney + multipledvalue;
+                oldmoney = newvalue;
+                PlayerPrefs.SetFloat("money", newvalue);
             }
         }
         public override void OnGUI()
         {
-            
+            if(renderedframes <= maxframes)
+            {
+                GUIStyle gUIStyle = new GUIStyle();
+                gUIStyle.fontSize = 18;
+                gUIStyle.fontStyle = FontStyle.Bold; 
+                gUIStyle.normal.textColor = Color.yellow;
+                GUI.Label(new Rect(Screen.width/2-15f,0,30f,20),texttoshow,gUIStyle);
+            } else
+            {
+                renderedframes++;
+            }
         }
         
     }
